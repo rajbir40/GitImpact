4f961540b23bf50bd2a6e09f079075f925844e76
Rajbir Singh <rajbir7528@gmail.com>
2025-06-19 00:06:58 +0530

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -68,8 +68,8 @@ app.use((err, req, res, next) => {
 // Start Server
 const PORT = process.env.PORT || 3000;
 
-// server.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
-export const handler = serverless(app);
+// export const handler = serverless(app);
